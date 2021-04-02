3f576d346ea2f6f3cb9646e71712ca93ef58501e
botfade <erikfaderik@gmail.com>
2021-04-02 19:13:03 +0700

    revisi no1

--- 1/soal1.sh
+++ 1/soal1.sh
@@ -3,20 +3,36 @@
 fLog=syslog.log
 uname=$(grep -oP "\(\K[^\)]+" ${fLog} | sort | uniq | tr ' ' '\n')
 
+1a(){
+    grep -oP "^(\S+\s){5}\K[^\ ]+" syslog.log       # jeenis log
+    grep -oP "^(\S+\s){6}\K[^\[\(]+" syslog.log     # pesan log
+    grep -oP "\(\K[^\)]+" syslog.log                # username
+}
+
 1b(){
-    echo "Error,Count" > error_message.csv
-    grep -i "ERROR" $fLog | grep -oP "^(\S+\s){6}\K[^\[\(]+" | sort | uniq -c | sort -nr | while read a b; do echo "$b,$a";done >> error_message.csv
+    grep -i "ERROR" $fLog | grep -oP "^(\S+\s){6}\K[^\[\(]+" | sort | uniq -c | sort -nr | while read a b; do echo "$b,$a";done
 }
 
 1c(){
-    echo "Username,INFO,ERROR" > user_statistic.csv
     echo "$uname" | while read u
     do
         echo "$u" | tr '\n' ','
         grep "($u)" $fLog | grep -o " INFO " | wc -w | tr '\n' ','
         grep "($u)" $fLog | grep -o " ERROR " | wc -w 
-    done >> user_statistic.csv
+    done
+}
+
+1d(){
+    echo "Error,Count" > error_message.csv
+    1b >> error_message.csv
+}
+
+1e(){
+    echo "Username,INFO,ERROR" > user_statistic.csv
+    1c >> user_statistic.csv
 }
 
 1b
 1c
+1d
+1e
