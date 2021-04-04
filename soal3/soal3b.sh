c2d9fa0795c4023f29211c6d2aad7ef687a9d722
siregarjonathan1 <73927472+siregarjonathan1@users.noreply.github.com>
2021-04-04 21:15:06 +0700

    Update soal3b.sh

--- soal3/soal3b.sh
+++ soal3/soal3b.sh
@@ -1,8 +1,53 @@
 #!/bin/bash
 
+
 tanggal=$(date +%d-%m-%Y)
 
-mkdir $tanggal
-cd $tanggal
+mkdir /home/jogar/Documents/praktikum1/$tanggal
+cd /home/jogar/Documents/praktikum1/$tanggal
 
-bash /home/jogar/Documents/praktikum1/soal3a.sh
+PWD="/home/jogar/Documents/praktikum1/$tanggal"
+
+i=1
+nF=1
+while [ $i -le 23 ]
+  do
+  
+  wget -O "$PWD/$nF.jpg" https://loremflickr.com/320/240/kitten -a "$PWD/Foto.log"
+
+  AWK=($(awk '/https:\/\/loremflickr.com\/cache\/resized\// {print $3}' "$PWD/Foto.log"))
+
+  j=0
+  beda=0
+
+  while [ $j -lt $(($i-1)) ]
+    do
+      if [ "${AWK[j]}" == "${AWK[$(($i-1))]}" ]
+	then
+          beda=1
+      fi
+
+      if [ $beda -eq 1 ]
+	then
+          rm "$PWD/$nF.jpg"
+	  nF=$nF-1
+	  break
+      fi
+
+  ((j+=1))
+  done
+	
+  if [[ -f "$PWD/$nF.jpg" ]]
+    then
+      if [ $nF -le 9 ]
+	then
+	  mv "$PWD/$nF.jpg" "$PWD/Koleksi_0$nF.jpg"
+	else
+	  mv "$PWD/$nF.jpg" "$PWD/Koleksi_$nF.jpg"
+
+      fi
+  fi
+
+  ((i+=1))
+  ((nF+=1))
+done
