23d8d88c646a8903653520c8df606bedafcbdb34
siregarjonathan1 <73927472+siregarjonathan1@users.noreply.github.com>
2021-04-04 19:50:12 +0700

    Update 3a.sh

--- soal3/3a.sh
+++ soal3/3a.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
-kol="Koleksi_"
-
 
 i=1
+nF=1
 while [ $i -le 23 ]
-do
-  if (( i < 10 ))
-   then nF="${kol}0${i}"
-  elif (( i <= 23 && i >= 10 ))
-   then nF="${kol}${i}"
-  fi
+  do
   
-wget -O "$nF" https://loremflickr.com/320/240/kitten -a Foto.log
+  wget -O "$nF.jpg" https://loremflickr.com/320/240/kitten -a Foto.log
 
-((i+=1))
+  AWK=($(awk '/https:\/\/loremflickr.com\/cache\/resized\// {print $3}' ./Foto.log))
+
+  j=0
+  beda=0
+
+  while [ $j -lt $(($i-1)) ]
+    do
+      if [ "${AWK[j]}" == "${AWK[$(($i-1))]}" ]
+	then
+          beda=1
+      fi
+
+      if [ $beda -eq 1 ]
+	then
+          rm "$nF.jpg"
+	  nF=$nF-1
+	  break
+      fi
+
+  ((j+=1))
+  done
+	
+  if [[ -f "$nF.jpg" ]]
+    then
+      if [ $nF -le 9 ]
+	then
+	  mv "$nF.jpg" "Koleksi_0$nF.jpg"
+	else
+	  mv "$nF.jpg" "Koleksi_$nF.jpg"
+
+      fi
+  fi
+
+  ((i+=1))
+  ((nF+=1))
 done
